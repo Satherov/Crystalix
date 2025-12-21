87f1d02fa8dd187d1150e84ea14bae32ce02a9fc
XFactHD <xfacthd@gmx.de>
2025-12-21 20:18:43 +0100

    Add FramedBlocks compatibility

---
+++ src/main/java/dev/satherov/crystalix/compat/framedblocks/CSFramedBlocksCompat.java
@@ -0,0 +1,37 @@
+package dev.satherov.crystalix.compat.framedblocks;
+
+import dev.satherov.crystalix.Crystalix;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+import xfacthd.framedblocks.api.camo.CamoContainerFactory;
+import xfacthd.framedblocks.api.util.FramedConstants;
+
+@Mod(value = Crystalix.MOD_ID)
+public final class CSFramedBlocksCompat {
+
+    public CSFramedBlocksCompat(IEventBus modBus) {
+        if (ModList.get().isLoaded("framedblocks")) {
+            Guarded.init(modBus);
+        }
+    }
+
+    static final class Guarded {
+
+        private static final DeferredRegister<CamoContainerFactory<?>> CAMO_FACTORIES = DeferredRegister.create(FramedConstants.CAMO_CONTAINER_FACTORY_REGISTRY_KEY, Crystalix.MOD_ID);
+
+        static final DeferredHolder<CamoContainerFactory<?>, CrystalixGlassCamoContainerFactory> FACTORY_CS_BLOCK = CAMO_FACTORIES.register(
+                "crystalix_glass", CrystalixGlassCamoContainerFactory::new
+        );
+
+        private static void init(IEventBus modBus) {
+            CAMO_FACTORIES.register(modBus);
+        }
+
+        private Guarded() { }
+    }
+}
---
+++ src/main/java/dev/satherov/crystalix/compat/framedblocks/CrystalixGlassCamoContainer.java
@@ -0,0 +1,53 @@
+package dev.satherov.crystalix.compat.framedblocks;
+
+import lombok.Getter;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.state.BlockState;
+
+import xfacthd.framedblocks.api.camo.block.AbstractBlockCamoContainer;
+import xfacthd.framedblocks.api.camo.block.AbstractBlockCamoContainerFactory;
+
+final class CrystalixGlassCamoContainer extends AbstractBlockCamoContainer<CrystalixGlassCamoContainer> {
+
+    private final @Getter int tintColor;
+
+    CrystalixGlassCamoContainer(BlockState state, int tintColor) {
+        super(state);
+        this.tintColor = tintColor;
+    }
+
+    @Override
+    public int getTintColor(BlockAndTintGetter level, BlockPos pos, int tintIdx) {
+        return tintColor;
+    }
+
+    @Override
+    public int getTintColor(ItemStack stack, int tintIdx) {
+        return tintColor;
+    }
+
+    @Override
+    public int hashCode() {
+        return content.hashCode() * 31 + Integer.hashCode(tintColor);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (!(obj instanceof CrystalixGlassCamoContainer other)) return false;
+        return content.equals(other.content) && tintColor == other.tintColor;
+    }
+
+    @Override
+    public String toString() {
+        return "CrystalixGlassCamoContainer{content=" + content + ",tintColor=" + Integer.toHexString(0xFF000000 | tintColor) + "}";
+    }
+
+    @Override
+    public AbstractBlockCamoContainerFactory<CrystalixGlassCamoContainer> getFactory() {
+        return CSFramedBlocksCompat.Guarded.FACTORY_CS_BLOCK.value();
+    }
+}
