ba6f5d99d0cab498a259d38c0022baa40b210e1a
Satherov <satherov@users.noreply.github.com>
2026-06-10 23:28:16 +0200

    update ghost state to block "enemies" instead of monsters - resolves #35
    Signed-off-by: Satherov <satherov@users.noreply.github.com>

--- src/main/java/dev/satherov/crystalix/common/properties/GhostState.java
+++ src/main/java/dev/satherov/crystalix/common/properties/GhostState.java
@@ -17,17 +17,22 @@ import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.util.StringRepresentable;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.animal.Animal;
 import net.minecraft.world.entity.animal.frog.Tadpole;
+import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.monster.Monster;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.phys.shapes.EntityCollisionContext;
 
 import com.mojang.serialization.Codec;
 
+import org.jspecify.annotations.Nullable;
+
 import io.netty.buffer.ByteBuf;
 
+import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
 @NothingNull
@@ -35,16 +40,16 @@ import java.util.function.Predicate;
 @RequiredArgsConstructor
 public enum GhostState implements StringRepresentable, PropertyEnum {
     // @formatter:off
-    BLOCK_ALL    (GenericLang.ALL,                   CXLanguage.TOOLTIP_GHOST_NONE,          ChatFormatting.DARK_GRAY,      false, _ -> false),
-    ALLOW_ALL    (GenericLang.ALL,                   CXLanguage.TOOLTIP_GHOST_ALL,           ChatFormatting.DARK_GRAY,      true,  _ -> true),
-    BLOCK_PLAYER (CXLanguage.PROPERTY_GHOST_PLAYER,  CXLanguage.TOOLTIP_GHOST_BLOCK_PLAYER,  ChatFormatting.AQUA,           false, context -> !(context.getEntity() instanceof Player)),
-    ALLOW_PLAYER (CXLanguage.PROPERTY_GHOST_PLAYER,  CXLanguage.TOOLTIP_GHOST_ALLOW_PLAYER,  ChatFormatting.AQUA,           true,  context ->   context.getEntity() instanceof Player),
-    BLOCK_MONSTER(CXLanguage.PROPERTY_GHOST_MONSTER, CXLanguage.TOOLTIP_GHOST_BLOCK_MONSTER, ChatFormatting.RED,            false, context -> !(context.getEntity() instanceof Monster)),
-    ALLOW_MONSTER(CXLanguage.PROPERTY_GHOST_MONSTER, CXLanguage.TOOLTIP_GHOST_ALLOW_MONSTER, ChatFormatting.RED,            true,  context ->   context.getEntity() instanceof Monster),
-    BLOCK_ANIMAL (CXLanguage.PROPERTY_GHOST_ANIMAL,  CXLanguage.TOOLTIP_GHOST_BLOCK_ANIMAL,  ChatFormatting.GREEN,          false, context -> !(context.getEntity() instanceof Animal)),
-    ALLOW_ANIMAL (CXLanguage.PROPERTY_GHOST_ANIMAL,  CXLanguage.TOOLTIP_GHOST_ALLOW_ANIMAL,  ChatFormatting.GREEN,          true,  context ->   context.getEntity() instanceof Animal),
-    BLOCK_ADULT  (CXLanguage.PROPERTY_GHOST_ADULT,   CXLanguage.TOOLTIP_GHOST_BLOCK_ADULT,   ChatFormatting.LIGHT_PURPLE,   false, context ->   context.getEntity() instanceof LivingEntity mob &&  mob.isBaby() ||  context.getEntity() instanceof Tadpole),
-    ALLOW_ADULT  (CXLanguage.PROPERTY_GHOST_ADULT,   CXLanguage.TOOLTIP_GHOST_ALLOW_ADULT,   ChatFormatting.LIGHT_PURPLE,   true,  context ->    context.getEntity() instanceof LivingEntity mob && !mob.isBaby() && !(context.getEntity() instanceof Tadpole)),
+    BLOCK_ALL    (GenericLang.ALL,                   CXLanguage.TOOLTIP_GHOST_NONE,          ChatFormatting.DARK_GRAY,      false, (_, _) -> false),
+    ALLOW_ALL    (GenericLang.ALL,                   CXLanguage.TOOLTIP_GHOST_ALL,           ChatFormatting.DARK_GRAY,      true,  (_, _) -> true),
+    BLOCK_PLAYER (CXLanguage.PROPERTY_GHOST_PLAYER,  CXLanguage.TOOLTIP_GHOST_BLOCK_PLAYER,  ChatFormatting.AQUA,           false, (e, _) -> !(e instanceof Player)),
+    ALLOW_PLAYER (CXLanguage.PROPERTY_GHOST_PLAYER,  CXLanguage.TOOLTIP_GHOST_ALLOW_PLAYER,  ChatFormatting.AQUA,           true,  (e, _) ->   e instanceof Player),
+    BLOCK_MONSTER(CXLanguage.PROPERTY_GHOST_MONSTER, CXLanguage.TOOLTIP_GHOST_BLOCK_MONSTER, ChatFormatting.RED,            false, (e, _) -> !(e instanceof Enemy)),
+    ALLOW_MONSTER(CXLanguage.PROPERTY_GHOST_MONSTER, CXLanguage.TOOLTIP_GHOST_ALLOW_MONSTER, ChatFormatting.RED,            true,  (e, _) ->   e instanceof Enemy),
+    BLOCK_ANIMAL (CXLanguage.PROPERTY_GHOST_ANIMAL,  CXLanguage.TOOLTIP_GHOST_BLOCK_ANIMAL,  ChatFormatting.GREEN,          false, (e, _) -> !(e instanceof Animal)),
+    ALLOW_ANIMAL (CXLanguage.PROPERTY_GHOST_ANIMAL,  CXLanguage.TOOLTIP_GHOST_ALLOW_ANIMAL,  ChatFormatting.GREEN,          true,  (e, _) ->   e instanceof Animal),
+    BLOCK_ADULT  (CXLanguage.PROPERTY_GHOST_ADULT,   CXLanguage.TOOLTIP_GHOST_BLOCK_ADULT,   ChatFormatting.LIGHT_PURPLE,   false, (e, _) ->   e instanceof LivingEntity mob &&  mob.isBaby() ||   e instanceof Tadpole),
+    ALLOW_ADULT  (CXLanguage.PROPERTY_GHOST_ADULT,   CXLanguage.TOOLTIP_GHOST_ALLOW_ADULT,   ChatFormatting.LIGHT_PURPLE,   true,  (e, _) ->   e instanceof LivingEntity mob && !mob.isBaby() && !(e instanceof Tadpole)),
     // @formatter:on
     ;
     
@@ -55,7 +60,7 @@ public enum GhostState implements StringRepresentable, PropertyEnum {
     private final SLTranslatable tooltip;
     private final ChatFormatting color;
     private final boolean allowed;
-    private final Predicate<EntityCollisionContext> predicate;
+    private final BiPredicate<@Nullable Entity, EntityCollisionContext> predicate;
     
     @Override
     public String getSerializedName() {
@@ -63,7 +68,7 @@ public enum GhostState implements StringRepresentable, PropertyEnum {
     }
     
     public boolean test(EntityCollisionContext context) {
-        return this.predicate.test(context);
+        return this.predicate.test(context.getEntity(), context);
     }
     
     @Override
